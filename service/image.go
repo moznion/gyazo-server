ce27a7d96efde3fd485f9b2dc8043b9455948c93
moznion <moznion@gmail.com>
2016-02-12 12:45:25 +0900

    Use io.Copy() instead hand-made logic
    Thank you for you suggestion @mattn

--- service/image.go
+++ service/image.go
@@ -17,21 +17,9 @@ func UploadImageForApp(fi multipart.File, s3 *aws.S3Info) (string, error) {
 	defer fo.Close()
 	defer os.Remove(fo.Name())
 
-	buf := make([]byte, 1024)
-	for {
-		n, err := fi.Read(buf)
-		if err != nil && err != io.EOF {
-			return "", err
-		}
-
-		if n == 0 {
-			break
-		}
-
-		_, err = fo.Write(buf[:n])
-		if err != nil {
-			return "", err
-		}
+	err = io.Copy(fo, fi)
+	if err != nil {
+		return "", err
 	}
 
 	return s3.Upload(fo, calcChecksum(fo)+".png", "image/png")
